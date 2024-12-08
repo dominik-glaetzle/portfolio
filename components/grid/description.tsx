8ca4079a32e22e146c339f5eae908979f4e946e2
smxkz <glatzledominik@gmail.com>
2024-12-08 19:38:25 +0100

    added animations

--- components/grid/description.tsx
+++ components/grid/description.tsx
@@ -1,43 +1,56 @@
-import memoji from '@/public/images/memoji.png';
-import profilepicture from '@/public/images/profile-picture.png';
-import Image from 'next/image';
-import Card from '../ui/card';
-import Head from 'next/head';
-import { motion } from 'framer-motion';
+"use client";
+import memoji from "@/public/images/memoji.png";
+import { motion } from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import Card from "../ui/card";
 
 export default function Description() {
-    return (
-        <>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&family=Poppins:ital,wght@1,900&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Moranga:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-            <Card className="flex flex-col justify-center gap-4 p-8">
-                <div className="relative size-14 overflow-hidden rounded-full sm:size-36">
-                    <Image
-                        src={memoji}
-                        alt="Dominik Glätzle"
-                        fill
-                        placeholder="blur"
-                        priority
-                        className="transition-transform duration-300 ease-in-out hover:scale-110"
-                    />
-                </div>
+      <Card className="flex flex-col justify-center p-8">
+        <motion.div
+          className="relative size-14 overflow-hidden rounded-full sm:size-36"
+          initial={{ scale: 0, opacity: 0, rotate: 250 }}
+          animate={{ scale: 1, opacity: 1, rotate: 360 }}
+          whileHover={{ scale: 1.8 }}
+          transition={{
+            duration: 0.7,
+            ease: "easeOut",
+          }}
+        >
+          <Image
+            src={memoji}
+            alt="Dominik Glätzle"
+            fill
+            placeholder="blur"
+            priority
+            className="transition-transform duration-300 ease-in-out"
+          />
+        </motion.div>
 
-                <p className="text-balance leading-relaxed">
-                    I&apos;m{' '}
-                    <span className="font-[Poppins] text-xl  font-black">
-                        Dominik
-                    </span>
-                    , a computer science student from Innsbruck, tyrol.{' '}
-                    <span className="hidden md:inline">
-                        I&apos;m interested in react (native), node.js and java.
-                    </span>
-                </p>
-            </Card>
-        </>
-    );
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9, ease: "easeOut" }}
+        >
+          <p className="text-balance leading-relaxed">
+            I&apos;m{" "}
+            <span className="font-[Poppins] text-xl font-black">dominik</span>,
+            a computer science student from Innsbruck, Tyrol.{" "}
+            <span className="hidden md:inline">
+              I&apos;m interested in React (Native), Node.js, and Java.
+            </span>
+          </p>
+        </motion.div>
+      </Card>
+    </>
+  );
 }
