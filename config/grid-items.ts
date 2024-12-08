13c11f53b6f5a532a6c4bb625f1b259d0c0f17f7
smxkz <glatzledominik@gmail.com>
2024-12-08 16:38:29 +0100

    reworked contact card, changed large layout

--- config/grid-items.ts
+++ config/grid-items.ts
@@ -9,7 +9,7 @@ export const gridItems: GridItem[] = [
     { i: 'article', component: GridComponents.Article },
     { i: 'description', component: GridComponents.Description },
     { i: 'location', component: GridComponents.Location },
-    { i: 'github', component: GridComponents.Github },
+    { i: 'contact', component: GridComponents.Contact },
     { i: 'linkedin', component: GridComponents.LinkedIn },
     { i: 'theme', component: GridComponents.Theme },
 ];
