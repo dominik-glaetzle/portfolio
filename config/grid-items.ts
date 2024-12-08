87dd3e2b90919a44893650ab9b86babaaf64f450
smxkz <glatzledominik@gmail.com>
2024-12-08 15:46:09 +0100

    delted commented code

--- config/grid-items.ts
+++ config/grid-items.ts
@@ -12,7 +12,4 @@ export const gridItems: GridItem[] = [
     { i: 'github', component: GridComponents.Github },
     { i: 'linkedin', component: GridComponents.LinkedIn },
     { i: 'theme', component: GridComponents.Theme },
-    // { i: 'project-2', component: GridComponents.SecondProject },
-    // { i: 'project-3', component: GridComponents.ThirdProject },
-    // { i: 'contact', component: GridComponents.Contact },
 ];
