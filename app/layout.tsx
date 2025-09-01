8ff127dc9855568f7ea13d2db5f5e96f13107e18
Dominik Glätzle <Dominik.Glaetzle@student.uibk.ac.at>
2025-09-01 16:06:58 +0200

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,52 @@
-import { siteConfig } from '@/config/site';
-import { calistoga, poppins } from '@/lib/fonts';
-import { cn } from '@/lib/utils';
-import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next';
-import { ThemeProvider } from './providers';
+import { siteConfig } from "@/config/site";
+import { calistoga, poppins } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/next";
+import type { Metadata } from "next";
+import { ThemeProvider } from "./providers";
 
-import './globals.css';
+import "./globals.css";
 
 export const metadata: Metadata = {
-    title: {
-        default: siteConfig.title,
-        template: '%s',
+  title: {
+    default: siteConfig.title,
+    template: "%s",
+  },
+  description: siteConfig.description,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
-    description: siteConfig.description,
-    robots: {
-        index: true,
-        follow: true,
-        googleBot: {
-            index: true,
-            follow: true,
-            noimageindex: true,
-            'max-video-preview': -1,
-            'max-image-preview': 'large',
-            'max-snippet': -1,
-        },
-    },
-    alternates: {
-        canonical: siteConfig.url,
-    },
+  },
+  alternates: {
+    canonical: siteConfig.url,
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <html lang='en' suppressHydrationWarning>
-            <body
-                className={cn(
-                    poppins.className,
-                    calistoga.variable,
-                    'bg-gray-100 antialiased dark:bg-dark-950'
-                )}>
-                <ThemeProvider attribute='class' enableSystem={false}>
-                    {children}
-                </ThemeProvider>
-                <Analytics />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          poppins.className,
+          calistoga.variable,
+          "bg-gray-100 antialiased dark:bg-dark-950",
+        )}
+      >
+        <ThemeProvider attribute="class" enableSystem={false}>
+          {children}
+        </ThemeProvider>
+        <Analytics />
+      </body>
+    </html>
+  );
 }
