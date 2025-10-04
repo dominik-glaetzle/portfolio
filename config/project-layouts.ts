a9796ea5e824b71d662e0836066562b04c584010
Dominik <dominik@glaetzle.io>
2025-10-04 21:39:10 +0200

    added new montis screen pictures + changed layout + career

--- config/project-layouts.ts
+++ config/project-layouts.ts
@@ -1,16 +1,15 @@
-import { Layout } from 'react-grid-layout';
+import { Layout } from "react-grid-layout";
 
 export const lgLayout: Layout[] = [
-    { i: 'logo', x: 0, y: 0, w: 1, h: 1 },
-    { i: 'home-screen', x: 1, y: 1, w: 1, h: 2 },
-    { i: 'notification-screen', x: 2, y: 1, w: 1, h: 2 },
-    { i: 'profile-screen', x: 3, y: 1, w: 1, h: 2 },
+  { i: "logo", x: 0, y: 0, w: 1, h: 1 },
+  { i: "home-screen", x: 1, y: 1, w: 1, h: 2 },
+  { i: "notification-screen", x: 2, y: 1, w: 1, h: 2 },
+  { i: "map-screen", x: 3, y: 1, w: 1, h: 2 },
 ];
 
 export const smLayout: Layout[] = [
-    { i: 'logo', x: 0, y: 1, w: 1, h: 1 },
-    { i: 'home-screen', x: 1, y: 1, w: 1, h: 2 },
-    { i: 'notification-screen', x: 0, y: 2, w: 1, h: 2 },
-    { i: 'profile-screen', x: 1, y: 3, w: 1, h: 2 },
-
+  { i: "logo", x: 0, y: 1, w: 1, h: 1 },
+  { i: "home-screen", x: 1, y: 1, w: 1, h: 2 },
+  { i: "notification-screen", x: 0, y: 2, w: 1, h: 2 },
+  { i: "map-screen", x: 1, y: 3, w: 1, h: 2 },
 ];
